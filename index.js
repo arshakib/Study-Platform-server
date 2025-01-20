3298ebe5b5e26398cac12ef9381651c44b3ef25c
shakib <atking2240@gmail.com>
2025-01-20 16:48:25 +0600

    all done

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
-require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://imaginative-chebakia-1dc907.netlify.app/",
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -28,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("study").collection("users");
     const sessionCollection = client.db("study").collection("sessions");
@@ -427,10 +431,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
